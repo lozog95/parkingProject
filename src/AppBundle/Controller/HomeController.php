62f2d141df24b74a643beba153d341a17ca3a3fb
lozog95 <lozog95@gmail.com>
2017-11-05 23:37:36 +0100

    added some react

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -9,7 +9,34 @@
 namespace AppBundle\Controller;
 
 
-class HomeController
-{
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
+class HomeController extends Controller
+{
+    /**
+     * @Route("/home/{name}")
+     */
+    public function showAction($name){
+       return $this->render('home/show.html.twig',['name'=>$name]);
+    }
+    /**
+     * @Route("/home/{name}/notes", name="home_show_notes")
+     * @Method("GET")
+     */
+    public function getNotes(){
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            'notes'=>$notes,
+        ];
+        return new JsonResponse($data);
+    }
 }
